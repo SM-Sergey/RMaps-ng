c73dce1b795b0d062dae9332b177a4fa10787291
Sergey Markov <sm@venus.ru>
2019-09-11 19:27:25 +0300

    Added some new map sources from SAS.Planet

--- mainActivity/src/main/java/com/sm/maps/applib/tileprovider/TileURLGeneratorCustom.java
+++ mainActivity/src/main/java/com/sm/maps/applib/tileprovider/TileURLGeneratorCustom.java
@@ -3,6 +3,8 @@ package com.sm.maps.applib.tileprovider;
 public class TileURLGeneratorCustom extends TileURLGeneratorBase {
 	private final static String X = "{x}";
 	private final static String Y = "{y}";
+	private final static String XD = "{xd}";
+	private final static String YD = "{yd}";
 	private final static String BX = "{bx}";
 	private final static String BY = "{by}";
 	private final static String RY = "{ry}";
@@ -29,10 +31,14 @@ public class TileURLGeneratorCustom extends TileURLGeneratorBase {
 		int bx = mod(x - (1 << (z-1)), (1 << z));
 		int by = (1 << z) - 1 - mod(y - (1 << (z-1)), (1 << z));
 
+		int xxd = x / 1024;
+		int yyd = y / 1024;
 
 		return getBase()
 				.replace(X, Integer.toString(x))
 				.replace(Y, Integer.toString(y))
+				.replace(XD, Integer.toString(xxd))
+				.replace(YD, Integer.toString(yyd))
 				.replace(BX, Integer.toString(bx))
 				.replace(BY, Integer.toString(by))
 				.replace(RY, Integer.toString((1<<z)-1-y))
